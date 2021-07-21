ed8015c703221bc1441d3785aeb89aed82e5a6a5
Karen Garcia <saykaren@gmail.com>
2021-07-21 15:28:34 -0600

    Adds Data in Users and origin for future React App

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource - Kindly Karen');
+  // res.send('respond with a resource - Kindly Karen');
+
+  var dataArray = [
+{name: "Karen", age: 21},
+{name: "John", age: 32},
+{name: "Bobby", age: 18},
+{name: "Ricky", age: 25},
+  ];
+
+  res.json({
+    data: dataArray
+  })
 });
 
 module.exports = router;
